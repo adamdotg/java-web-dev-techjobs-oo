e10bb3094a792fc75cc264c25288eb2429f4dc77
adamdotg <adamdotg@gmail.com>
2020-02-02 12:50:09 -0600

    part 1 & 2

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.Objects;
+
 public class PositionType {
     private int id;
     private static int nextId = 1;
@@ -16,9 +18,39 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-
+    @Override
+    public String toString(){
+        return value;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!( o instanceof PositionType)) return false;
+        PositionType positiontype = (PositionType) o;
+        return getId() == positiontype.getId();
+    }
+//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+//        if (this == o) return true;
+//        if (!(o instanceof Employer)) return false;
+//        Employer employer = (Employer) o;
+//        return getId() == employer.getId();
+//    }    *--copied from Employer class--* generate equals() gave questionable results
+//-------------Questionable results start-------------
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        PositionType that = (PositionType) o;
+//        return id == that.id;
+//    }
+//-------------Questionable results end-------------
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     // Getters and Setters:
 
